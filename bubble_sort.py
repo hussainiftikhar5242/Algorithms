03f761516d8a8b025d35c2de3f93b95650e96046
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2022-10-07 21:54:00 +0500

    Updated bubbleSort.py
    Added by me

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,23 +1,11 @@
-def bubblesort(arr):
-    n=len(arr)
-    swapped = False
-    for i in range (n-1):
-       
-        for j in range (0,n-i-1):
-           
-            if arr[j]>arr[j+1]:
-                swapped = True
-                temp=arr[j]
-                arr[j]=arr[j+1]
-                arr[j+1]=temp
+def bubbleSort(list):
+    for num in range(len(list)):
+        for i in range(num):
+            if list[i]>list[i+1]:
+                temp = list[i]
+                list[i] = list[i+1]
+                list[i+1] = temp
 
-        if not swapped:
-            return    
-  
-
-
-arr=[23,21,34,32,12,10,10]
-bubblesort(arr)
-print("Sorted array :")
-for i in range(len(arr)):
-    print(arr[i],end=" ")
+list = [14,46,43,27,57,41,45,21,70]
+bubbleSort(list)
+print(list)
